d916495e167d6dd0fc418bde50490ff349a27bc5
ExpectoPatronum97 <jovannikolic1@hotmail.com>
2018-05-16 22:41:33 +0200

    Don't panic EP1

---
+++ Medium/Don't_panic_EP1.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main()
+{
+    int nbFloors, exitFloor, exitPos, nbElevators;
+    scanf("%d%*d%*d%d%d%*d%*d%d", &nbFloors, &exitFloor, &exitPos, &nbElevators); // %*d ignores input
+
+    int elevators[nbElevators];
+    for (int i = 0; i < nbElevators; i++)
+    {
+        int elevatorFloor;
+        int elevatorPos;
+        scanf("%d%d",&elevatorFloor, &elevatorPos);
+        elevators[elevatorFloor] = elevatorPos;
+    }
+    elevators[exitFloor] = exitPos;
+    // game loop
+    while (1)
+     {
+        int cloneFloor, clonePos;
+        string direction;
+        cin >> cloneFloor >> clonePos >> direction; cin.ignore();
+
+        if (direction == "LEFT")
+        {
+            cout << (elevators[cloneFloor] > clonePos ? "BLOCK" : "WAIT") << endl;
+        }
+        else if (direction == "RIGHT")
+        {
+            cout << (elevators[cloneFloor] < clonePos ? "BLOCK" : "WAIT") << endl;
+        }
+        else
+            cout << "WAIT" << endl;
+    }
+}
